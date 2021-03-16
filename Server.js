acdf7461e7686559c5b68595cf7bbd837806a8e1
arnoldoJose <aolivasgonzales73@gmail.com>
2021-03-16 10:46:23 -0600

    new port

--- Server.js
+++ Server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
-let port = 4000;
 
 const path = require('path');
 let bodyParser = require('body-parser');
 const Cors = require('cors');
 const { conectionDB } = require('./DB/DBConnect');
-
+require('./Config/config');
 app.use(Cors());
 
 conectionDB();
@@ -26,4 +25,4 @@ app.use(require('./Routes/Routes'));
 
 app.use(express.static(path.join(__dirname,'storage/image')));
 
-app.listen(port, () => console.log("server online"));
+app.listen(process.env.PORT, () => console.log("server online"));
