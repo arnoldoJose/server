3e758ed8ff1ac421142cc35b074c344bac466dea
arnoldoJose <aolivasgonzales73@gmail.com>
2021-03-18 09:54:29 -0600

    corrigiendo error en el reque otra vess

--- CaseLoans/createLoans/createLoans.js
+++ CaseLoans/createLoans/createLoans.js
@@ -6,7 +6,7 @@ const createLoan = (req,res) => {
 const { name_user, mobile_user, image_book, name_book, return_loan } = req.body;
   const loan = createLoans();
   loan.name_book = name_book;
-  loan.image_book = book_cover;
+  loan.image_book = image_book;
   loan.name_user = name_user;
   loan.mobile_user = mobile_user;
   loan.date_loan = new Date().toLocaleString();
