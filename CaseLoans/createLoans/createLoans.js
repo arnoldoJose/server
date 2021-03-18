c778e1ac823aa3cb741c17d412928547b82d1bfa
arnoldoJose <aolivasgonzales73@gmail.com>
2021-03-18 09:44:07 -0600

    save loans

--- CaseLoans/createLoans/createLoans.js
+++ CaseLoans/createLoans/createLoans.js
@@ -3,15 +3,18 @@ const { createLoans } = require('../../Repositories/loansRepositori');
 
 const createLoan = (req,res) => {
 
-const { name_user,mobile_user,image_book,name_book, return_loan } = req.body;
+const { name_user,mobile_user,book_cover,name_book, return_loan } = req.body;
   const loan = createLoans();
   loan.name_book = name_book;
-  loan.image_book = image_book;
+  loan.image_book = book_cover;
   loan.name_user = name_user;
   loan.mobile_user = mobile_user;
   loan.date_loan = new Date().toLocaleString();
   loan.return_date = return_loan;
 //crear otro campo llamado cantidad
+
+  loan.save();
+
   res.json( loan );
 
 }
