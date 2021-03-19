cde653f48ff92f292fecb35c46d1030d081a4d4f
arnoldoJose <aolivasgonzales73@gmail.com>
2021-03-19 09:28:50 -0600

    change url db

--- DB/DBConnect.js
+++ DB/DBConnect.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config({path:'development.env'})
 
-// process.env.DB_URL,
+// "mongodb://localhost:27017/library"
+
 const conectionDB = () => {
   mongoose.connect(
-    "mongodb://localhost:27017/library",
+    process.env.DB_URL,
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err) => {
       if (!err) {
