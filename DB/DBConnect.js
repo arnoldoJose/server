15e8104e5e66beb7b059e3d5c3a6d1d0b42d8769
arnoldoJose <aolivasgonzales73@gmail.com>
2021-03-19 09:25:21 -0600

    probando reques nuevos

--- DB/DBConnect.js
+++ DB/DBConnect.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config({path:'development.env'})
 
+// process.env.DB_URL,
 const conectionDB = () => {
   mongoose.connect(
-  process.env.DB_URL,
+    "mongodb://localhost:27017/library",
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err) => {
       if (!err) {
--- Repositories/loansRepositori.js
+++ Repositories/loansRepositori.js
@@ -1,6 +1,6 @@
 const Loans = require('../DB/Models/Loans');
 
-const getRepositoriLoans = async() => await Loans.find();
+const getRepositoriLoans = async () => await Loans.find({ reservation_state: "not-assigned" });
 const createLoans = () => new Loans();
 
 module.exports = { createLoans, getRepositoriLoans };
