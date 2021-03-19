0bfcf46beedf3347bcf72e75848d2c7fef4588ce
arnoldoJose <aolivasgonzales73@gmail.com>
2021-03-19 09:35:34 -0600

    probando reques otra ves

--- Repositories/loansRepositori.js
+++ Repositories/loansRepositori.js
@@ -1,6 +1,6 @@
 const Loans = require('../DB/Models/Loans');
 
-const getRepositoriLoans = async () => await Loans.find({ reservation_state: "not-assigned" });
+const getRepositoriLoans = async () => await Loans.find({ reservation_state: "assigned" });
 const createLoans = () => new Loans();
 
 module.exports = { createLoans, getRepositoriLoans };
