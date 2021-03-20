2e2b5e79adee0965b0f0aef83079e0eaabb07df0
arnoldoJose <aolivasgonzales73@gmail.com>
2021-03-20 10:08:10 -0600

    change reques

--- Repositories/loansRepositori.js
+++ Repositories/loansRepositori.js
@@ -1,6 +1,6 @@
 const Loans = require('../DB/Models/Loans');
 
-const getRepositoriLoans = async () => await Loans.find({ reservation_state: "assigned" });
+const getRepositoriLoans = async () => await Loans.find({ reservation_state: "assigned", return_state: "not-assigned"});
 const getReturnLoan = async () => await Loans.find({return_state: "not-assigned"});
 const getAllReservetaions = async () =>  await Loans.find({ reservation_state: "not-assigned" });
 const createLoans = () => new Loans();
