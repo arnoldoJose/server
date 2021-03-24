1d31d6a3f108d549d91b02bc8f0c2091e74af56c
arnoldoJose <aolivasgonzales73@gmail.com>
2021-03-24 09:33:00 -0600

    change logic server

--- Server.js
+++ Server.js
@@ -1,5 +1,27 @@
-const Server = require('./LogicServer/logicServer');
+const express = require("express");
+const app = express();
+const session = require("express-session");
+let bodyParser = require("body-parser");
+const Cors = require("cors");
+const { conectionDB } = require("./DB/DBConnect");
+require("./Config/config");
 
-const server = new Server();
+  app.use(Cors());
 
-server.execute();
+  app.use(
+    session({
+      secret: "secrexxx12",
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: true },
+    })
+  );
+  conectionDB();
+
+  app.use(bodyParser.json());
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(require("./Routes/Routes"));
+
+  app.listen(process.env.PORT, () => console.log("server online"));
