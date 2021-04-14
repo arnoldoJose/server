a961d2f7b37c2ec01e40a1953b2e8bfe9d6940e3
arnoldoJose <aolivasgonzales73@gmail.com>
2021-04-14 17:20:59 -0600

    change message

--- twiliosms/sendSms.js
+++ twiliosms/sendSms.js
@@ -2,15 +2,19 @@ const { smsSid,smsToken } = require("../Config/config");
 const client = require("twilio")(smsSid,smsToken);
 
 const sendMessage = async (req, res) => {
-   let { phone } = req.query;
-   
-  let data = await client.messages.create({
-    to: `+505${phone}`,
-    from: "+15852095296",
-    body: "mi primer mensaje",
-  });
+   try {
+     let { phone } = req.query;
 
- res.json(data.sid);
+     let data = await client.messages.create({
+       to: `+505${phone}`,
+       from: "+15852095296",
+       body: "mi primer mensaje",
+     });
+
+     res.json(data.sid);
+   } catch (error) {
+     console.log(error);
+   }
 };
 
 module.exports = { sendMessage };
